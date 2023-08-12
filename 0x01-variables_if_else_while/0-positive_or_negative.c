f392724b3850a68b8937800069e9a31606b1d3e2
coolguy-1 <mphomoetisc@gmail.com>
2023-08-12 02:42:46 -0700

    0-positive_or_negative.c commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive");
+		printf("%d is positive\n");
 	}
 	else if ( n < 0)
 	{
-		printf("%d is negative");
+		printf("%d is negative\n");
 	}
 	else
 	{
-		printf("0 is zero");
+		printf("0 is zero\n");
 	}
 	return (0);
 }
